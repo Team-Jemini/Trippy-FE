8e98271cda8bed78ee3d91252d393489a394a700
i-soj-ng <jeong4530@gmail.com>
2025-08-03 23:09:39 +0900

    feat : 비밀번호 입력 뷰 연동

--- trippy-client/src/views/JoinView.vue
+++ trippy-client/src/views/JoinView.vue
@@ -3,12 +3,15 @@ import { ref, computed } from "vue";
 
 import InputForm from "@/components/personal-accounts/import-accounts/InputForm.vue";
 import SMSCertification from "@/components/personal-accounts/import-accounts/SMSCertification.vue";
+import PasswordInput from "@/components/common/inputs/PasswordInput.vue";
 
 const views = [
   { component: InputForm },
   { component: SMSCertification },
+  { component: PasswordInput, props: { title: "새 비밀번호 입력" }},
+  { component: PasswordInput, props: { title: "비밀번호 재입력" }},
 ];
-const currentIndex = ref(0);
+const currentIndex = ref(2);
 
 const currentView = computed(() => views[currentIndex.value]);
 
@@ -23,6 +26,7 @@ function goNext() {
   <main class="w-full bg-white h-full">
     <component
       :is="currentView.component"
+      :key="currentIndex"
       v-bind="currentView.props"
       @next="goNext"
     />
