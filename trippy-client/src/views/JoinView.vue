c78b67d091a44d1faa3c31b8efbfcaa06bd3101c
i-soj-ng <jeong4530@gmail.com>
2025-08-03 23:13:12 +0900

    feat : 로딩 뷰 연동

--- trippy-client/src/views/JoinView.vue
+++ trippy-client/src/views/JoinView.vue
@@ -4,12 +4,14 @@ import { ref, computed } from "vue";
 import InputForm from "@/components/personal-accounts/import-accounts/InputForm.vue";
 import SMSCertification from "@/components/personal-accounts/import-accounts/SMSCertification.vue";
 import PasswordInput from "@/components/common/inputs/PasswordInput.vue";
+import LoadingView from "@/components/common/loading/LoadingView.vue";
 
 const views = [
   { component: InputForm },
   { component: SMSCertification },
   { component: PasswordInput, props: { title: "새 비밀번호 입력" }},
   { component: PasswordInput, props: { title: "비밀번호 재입력" }},
+  { component: LoadingView, props: { description: "인증 수단을 등록하는 중입니다..." }},
 ];
 const currentIndex = ref(2);
 
