c115d73b798efefd6e661d82a8651a68513ab489
kang-yesung <rkddptjd5228@gmail.com>
2025-08-08 16:16:41 +0900

    chore api 호출 함수명 변경

--- trippy-client/src/api/groupAccount.js
+++ trippy-client/src/api/groupAccount.js
@@ -6,7 +6,7 @@ const BASE_URL = "/group-account";
 const userId = ref(1);
 
 export default {
-  async CreateAccounId(accountName, email, mainAccountId) {
+  async createAccounId(accountName, email, mainAccountId) {
     const res = await api.post(`${BASE_URL}/create?userId=${userId.value}`, {
       accountName,
       email,
