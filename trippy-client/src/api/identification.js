436c87e5dfec35f80717f468a5710cc1a039a7b3
bangbang444 <kim1228com@naver.com>
2025-08-11 16:41:13 +0900

    refactor: http body 작성법 변경

--- trippy-client/src/api/identification.js
+++ trippy-client/src/api/identification.js
@@ -1,44 +1,21 @@
-import axios from "axios";
 import api from "./index";
 
 export const addResidentCard = async (userId, data) => {
-  const response = await api.post(
-    `/residentCard`,
-    {
-      imgUrl: data.imgUrl,
-      name: data.name,
-      identity: data.identity,
-      address: data.address,
-      resIssueDate: data.resIssueDate,
+  const response = await api.post(`/residentCard`, data, {
+    headers: {
+      "X-USER-ID": userId,
     },
-    {
-      headers: {
-        "X-USER-ID": userId,
-      },
-    },
-  );
+  });
   return response.data;
 };
 
 export const addPassport = async (userId, data) => {
   //TODO: 여권 사진
-  const response = await api.post(
-    `/passport`,
-    {
-      passportNumber: data.passportNumber,
-      nameKr: data.nameKr,
-      nameEn: data.nameEn,
-      birthDate: data.birthDate,
-      gender: data.gender,
-      countryCode: data.countryCode,
-      expireDate: data.expireDate,
+  const response = await api.post(`/passport`, data, {
+    headers: {
+      "X-USER-ID": userId,
     },
-    {
-      headers: {
-        "X-USER-ID": userId,
-      },
-    },
-  );
+  });
 
   return response.data;
 };
