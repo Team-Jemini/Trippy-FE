a71d00fcec88e7916b0fd01d0c2c6ed6c1083b17
kang-yesung <rkddptjd5228@gmail.com>
2025-08-12 15:38:42 +0900

    feat 알림이 없을 경우 조건 처리

--- trippy-client/src/views/Notifications.vue
+++ trippy-client/src/views/Notifications.vue
@@ -1,6 +1,7 @@
 <script setup>
 import NotificationCard from "@/components/home/NotificationCard.vue";
 import { useNotificationStore } from "@/stores/notificationsStore";
+import { Icon } from "@iconify/vue";
 import { onMounted, ref } from "vue";
 
 const notificationStore = useNotificationStore();
@@ -9,17 +10,24 @@ const notificationData = ref([]);
 
 onMounted(async () => {
   await notificationStore.getNotifications();
-  notificationData.value = notificationStore.notificationList;
+  // notificationData.value = notificationStore.notificationList;
 });
 </script>
 
 <template>
   <div class="h-full w-full overflow-scroll mt-[-1rem] [&::-webkit-scrollbar]:hidden mb-12">
-    <NotificationCard
-      v-for="notification in notificationData"
-      :key="notification.notiId"
-      :notification="notification"
-    />
+    <div v-if="notificationData.length > 0">
+      <NotificationCard
+        v-for="notification in notificationData"
+        :key="notification.notiId"
+        :notification="notification"
+      />
+    </div>
+
+    <div v-else class="flex flex-col justify-center items-center h-full mt-[-3rem]">
+      <Icon icon="material-symbols:notifications-rounded" class="size-24 text-gray-400" />
+      <p class="text-gray-400 body1">알림이 없습니다.</p>
+    </div>
   </div>
 </template>
 
