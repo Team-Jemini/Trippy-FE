082b776cad763e1359d1ba70fe4e51c4d74890d1
i-soj-ng <jeong4530@gmail.com>
2025-08-13 17:14:23 +0900

    feat : 인증번호 요청 API 추가

--- trippy-client/src/api/user.js
+++ trippy-client/src/api/user.js
@@ -2,7 +2,7 @@ import api from './index';
 
 export const postUser = async (data) => {
   try {
-    const response = await api.post(`/user/signup`, data);
+    const response = await api.post("/user/signup", data);
     return response.data;
   } catch (error) {
     console.error("회원 등록 실패", error);
@@ -12,11 +12,23 @@ export const postUser = async (data) => {
 
 export const postPassword = async (data) => {
   try {
-    const response = await api.post(`/user/password`, {
+    const response = await api.post("/user/password", {
       password: data
     });
     return response.data;
   } catch (error) {
     return error;
   }
-}
+};
+
+export const postVerifyRequest = async (phoneNumber) => {
+  try {
+    const response = await api.post("/user/phoneNumber", {
+      phoneNumber: phoneNumber
+    });
+
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
