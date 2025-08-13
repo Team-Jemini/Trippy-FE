8b6ceb082100b432d51da69f132cb3e0bfef256b
kang-yesung <rkddptjd5228@gmail.com>
2025-08-13 13:28:28 +0900

    feat store 생성

---
+++ trippy-client/src/stores/groupTransferStore.js
@@ -0,0 +1,9 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useGroupTransferStore = defineStore("groupTransfer", () => {
+  const loading = ref(false);
+  const error = ref(null);
+
+  return { loading, error };
+});
