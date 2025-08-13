5049dcaeaeed377302a787d75ea166fde87f5c86
i-soj-ng <jeong4530@gmail.com>
2025-08-13 15:45:58 +0900

    hotfix : registerServiceWorker 파일 수정

--- trippy-client/src/registerServiceWorker.js
+++ trippy-client/src/registerServiceWorker.js
@@ -1,7 +1,7 @@
 import { register } from 'register-service-worker';
 
 // if (import.meta.env.NODE_ENV === 'production') {
-  register(`${import.meta.env.BASE_URL}service-worker.js`, {
+  register(`/service-worker.js`, {
     ready () {
       console.log(
         'App is being served from cache by a service worker.\n' +
