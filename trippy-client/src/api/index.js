299737ee4bb0eff1603f329d33161d9be64462a4
kang-yesung <rkddptjd5228@gmail.com>
2025-08-13 09:41:40 +0900

    chore baseURL 수정

--- trippy-client/src/api/index.js
+++ trippy-client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://10.10.0.22:8080/",
+  baseURL: "http://localhost:8080/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
