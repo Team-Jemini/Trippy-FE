c269ae9c040cc1d94a4950f2492e46f27c6f7a83
hellozo0 <ahju0011@gmail.com>
2025-08-13 18:46:13 +0900

    feat : api 연동 형식 변경

--- trippy-client/src/api/voucher.js
+++ trippy-client/src/api/voucher.js
@@ -3,21 +3,19 @@ import api from "@/api/index.js";
 export const fetchVoucher = async () => {
   try {
     const response = await api.get(`/voucher`);
-
-    return response.data;
+    return response.data.data;
   } catch (error) {
-    console.error("바우처(숙소,관광) 전체 조회 실패", error);
+    console.error("바우처 전체 조회 실패", error);
     throw error;
   }
 };
 
-export const fetchAccommodationDetail = async (accommocationId) => {
+export const fetchAccommodationDetail = async (accommodationId) => {
   try {
-    const response = await api.get(`/voucher/accommodation/${accommocationId}`);
-
+    const response = await api.get(`/accommodation/${accommodationId}`);
     return response.data;
   } catch (error) {
-    console.error("숙소예약 상세 조회 실패", error);
+    console.error("숙소 상세 조회 실패", error);
     throw error;
   }
 };
